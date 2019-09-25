97f7f602659a3aa5295865ca90896371853cecd1
emilymech <emily.mech@gmail.com>
2019-09-25 14:08:21 -0500

    add initial count scripts - not finalized

---
+++ word_v_world/count_params.py
@@ -0,0 +1,4 @@
+param2requests = {'part': [0, 1, 2, 3, 4, 5, 6]}  # pass unique integers to each machine to allow unique start_id
+
+param2default = {'part': 0,
+                 'num_machines': 7, 'param_name': "counts"}
