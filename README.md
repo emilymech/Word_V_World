4fe2446e255e6613c9c772d45eaa43ec1987f5c2
phueb <info@philhuebner.com>
2019-09-26 18:57:21 -0500

    added dummy input xml file which can be used for development - rather than having to use a big xml file

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Word_V_World
 
+## Background
+
 This is a new project that uses Wikipedia as a corpus from which to extract word co-occurrences. These co-occurrences will later be utilized as experimental stimuli to understand how regularities in language interact with our knowledge about regularities in the world.
+
+
+## To-do
+
+* better documentation of how corporar were generated
+    * move some of the important variables from the wiki-extractor code to params.py
+    * add other important variables related to corpus processing to params.py
--- word_v_world/config.py
+++ word_v_world/config.py
@@ -1,8 +1,14 @@
 from pathlib import Path
+import sys
 
 
 class RemoteDirs:
     root = Path('/Volumes/research_data') / 'Word_V_World'
+
+    if sys.platform == 'linux':
+        print('Detected Linux. Changing path to remote root directory')
+        root = Path('/media/research_data') / 'Word_V_World'
+
     runs = root / 'runs'
 
 
@@ -17,3 +23,7 @@ class Global:
     min_article_length = 1
     input = 'data/enwiki-20190920-pages-articles-multistream.xml.bz2'
     output = 'output'  # name of directory where output of wiki extractor script is saved
+
+    if not Path(input).exists():
+        print('WARNING: Using dummy xml file as input file because {} could not be found'.format(input))
+        input = 'dummy_input.xml'  # ~250 random wiki articles
