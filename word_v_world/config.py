9b802ccc5bf45f02da016974a9b5b0f17d4a5bcd
phueb <info@philhuebner.com>
2019-09-27 09:39:05 -0500

    add note about paths

--- word_v_world/config.py
+++ word_v_world/config.py
@@ -5,9 +5,15 @@ is_linux = sys.platform == 'linux'
 
 
 class RemoteDirs:
+    """
+    Typically, when using LudwigCluster to execute code, root should always be /media/research_data
+     regardless of user's OS.
+    However, when user intends to execute code on host only, and intends to retrieve data from shared drive,
+     and has MacOS, root must be changed to something like /Volumes/research_data
+
+    """
     root = Path('/{}/research_data'.format('media' if is_linux else 'Volumes')) / 'Word_V_World'
     runs = root / 'runs'
-
     wiki = Path('/{}/research_data'.format('media' if is_linux else 'Volumes')) / 'CreateWikiCorpus'
 
 
