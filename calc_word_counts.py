98db02d017ba9ed8bb5a4dd471c1ee2b41fef658
emilymech <emily.mech@gmail.com>
2019-10-16 14:33:45 -0500

    add custom tokenizer

--- calc_word_counts.py
+++ calc_word_counts.py
@@ -1,32 +1,33 @@
 import matplotlib.pyplot as plt
 import operator
+import re
 
 
-from word_v_world.articles import get_paths_to_articles, generate_articles
+# from word_v_world.articles import get_paths_to_articles, generate_articles
 from word_v_world.stats import make_word2frequency
-from tokenize_articles import tokens
+import tokenize_articles
 
-NUM_ARTICLES = 50  # total articles in 1/7 corpus evaluated here: 847,505 (10/11/19), 5934163 in total
-NUM_WORDS = 200
+NUM_ARTICLES = 1  # total articles in 1/7 corpus evaluated here: 847,505 (10/11/19), 5934163 in total
+NUM_WORDS = 100
 PARAM2REQUESTS = {'part': [0, 1, 2, 3, 4, 5, 6],
                   'num_machines': [7],
                   'input_file_name': ['enwiki-20190920-pages-articles-multistream.xml.bz2']}
-# user can overwrite default by overwriting None
 # TODO need to ask Phil about how to change this from "None" the right way ^
-
-paths_to_articles = []
-for p in get_paths_to_articles(param2requests=PARAM2REQUESTS):
-    paths_to_articles.append(p)
 
 
 # make a list of words in all articles
-all_words = []
-for article in generate_articles(paths_to_articles, num_articles=NUM_ARTICLES):
-    words = tokens
-    all_words += words
+all_words = tokenize_articles.tokenize(NUM_ARTICLES)
 
 # count word frequencies
 w2f = make_word2frequency(all_words)
+
+# count the enters in the corpus
+count_enters = 0
+for word in all_words:
+    if '\n':
+        count_enters += 1
+print("Number of enters:", count_enters)
+
 
 # print most frequent words and plot them (at cut-offs of: 4096, 8192, 16384, 32768)
 for n, (w, f) in enumerate(w2f.items()):  # dictionary is ordered by freq
@@ -36,44 +37,40 @@ for n, (w, f) in enumerate(w2f.items()):  # dictionary is ordered by freq
     print('{:<20} {}'.format(w, f))
 
 
-def word_freq():
-    freq_list = make_word2frequency(all_words)
-    freq = enumerate(freq_list.items())
-    sorted_wiki_freqs = (sorted(freq.items(), key=operator.itemgetter(1)))[::-1]
-    word = "the"
-    just_the_occur = []
-    just_the_rank = []
-    word_rank = 0
-    word_frequency = 0
+# make a frequency plot
+freq_list = make_word2frequency(all_words)
+sorted_wiki_freqs = (sorted(freq_list.items(), key=operator.itemgetter(1)))[::-1]
+word = "at"
+just_the_occur = []
+just_the_rank = []
+word_rank = 0
+word_frequency = 0
 
-    entry_num = 1
-    for entry in sorted_wiki_freqs:
+entry_num = 1
+for entry in sorted_wiki_freqs:
 
-        if entry[0] == word:
-            word_rank = entry_num
-            word_frequency = entry[1]
+    if entry[0] == word:
+        word_rank = entry_num
+        word_frequency = entry[1]
 
-        just_the_rank.append(entry_num)
-        entry_num += 1
-        just_the_occur.append(entry[1])
+    just_the_rank.append(entry_num)
+    entry_num += 1
+    just_the_occur.append(entry[1])
 
-    plt.title("Word Frequencies in " + sorted_wiki_freqs)
-    plt.ylabel("Total Number of Occurrences")
-    plt.xlabel("Rank of word(\"" + word + "\" is rank " + str(word_rank) + ")")
-    plt.loglog(
-        just_the_rank,
-        just_the_occur,
-        basex=10
-    )
-    plt.scatter(
-        [word_rank],
-        [word_frequency],
-        color="orange",
-        marker="*",
-        s=100,
-        label=word
-    )
-    plt.show()
-
-
-
+plt.title("Word Frequencies in Wiki Corpus")
+plt.ylabel("Total Number of Occurrences")
+plt.xlabel("Rank of word(\"" + word + "\" is rank " + str(word_rank) + ")")
+plt.loglog(
+    just_the_rank,
+    just_the_occur,
+    basex=10
+)
+plt.scatter(
+    [word_rank],
+    [word_frequency],
+    color="orange",
+    marker="*",
+    s=100,
+    label=word
+)
+#plt.show()
--- tokenize_articles.py
+++ tokenize_articles.py
@@ -6,74 +6,71 @@ make sure to  do this once:
 python -m spacy download en_core_web_sm
 """
 
+import re
 import spacy
-from spacy.tokens import Doc
+
 
 from word_v_world.articles import generate_articles, generate_articles_from_slice, get_paths_to_articles
-
-NUM_ARTICLES = 1
-
-nlp = spacy.load("en_core_web_sm")
-
-
-class WhitespaceTokenizer(object):
-    def __init__(self, vocab):
-        self.vocab = vocab
-
-    def __call__(self, text):
-        words = text.split(' ')
-        spaces = [True] * len(words)
-        # All tokens 'own' a subsequent space character in this tokenizer
-        return Doc(self.vocab, words=words, spaces=spaces)
-
+from spacy.tokenizer import Tokenizer
 
 
 PARAM2REQUESTS = {'part': [0, 1, 2, 3, 4, 5, 6],
                   'num_machines': [7],
                   'input_file_name': ['enwiki-20190920-pages-articles-multistream.xml.bz2']}
 # user can overwrite default by overwriting None
-# TODO need to ask Phil about how to change this from "None" the right way ^
 
 
-paths_to_articles = []
-for p in get_paths_to_articles(param2requests=PARAM2REQUESTS):
-    paths_to_articles.append(p)
+def get_path():
+    paths_to_articles = []
+    for p in get_paths_to_articles(param2requests=PARAM2REQUESTS):
+        paths_to_articles.append(p)
+    return paths_to_articles
 
 
-# loop over articles, tokenizing each with built-in tokenizer
-for article in generate_articles(paths_to_articles, num_articles=NUM_ARTICLES):
+def tokenize(n):
+    paths_to_articles = get_path()
+    all_tokens = []
 
-    # tokenize article
-    doc = nlp(article)  # tokenization, tagging, ner, etc...
+    custom_nlp = spacy.load("en_core_web_sm")
+    hyphen_contraction_re = re.compile(r"[A-Za-z]+(-|')[A-Za-z\.]+")
+    prefix_re = spacy.util.compile_prefix_regex(custom_nlp.Defaults.prefixes)
+    infix_re = spacy.util.compile_infix_regex(custom_nlp.Defaults.infixes)
+    suffix_re = spacy.util.compile_suffix_regex(custom_nlp.Defaults.suffixes)
+    custom_nlp.tokenizer = Tokenizer(custom_nlp.vocab,
+                                     prefix_search=prefix_re.search,
+                                     infix_finditer=infix_re.finditer,
+                                     suffix_search=suffix_re.search,
+                                     token_match=hyphen_contraction_re.match)
 
-    tokens = [t.text.strip('.').strip(',').strip(')').strip('(').strip('/').lower() for t in doc]
-    print(tokens)
-    print(len(tokens))
-    print()
+    # loop over articles, tokenizing each with built-in tokenizer
+    i = 0  # for tracking print process
+    for article in generate_articles(paths_to_articles, num_articles=n):
+
+        # tokenize article
+        doc = custom_nlp(article)  # tokenization, tagging, ner, etc...
+        tokens = [t.text.lower() for t in doc]
+        all_tokens += tokens
+
+        # track print process
+        i += 1
+        if i % 100 == 0:
+            print("Finished {} articles".format(i))
+
+        # for debugging, print tokens in sample articles (only when num articles is small)
+        print(tokens)
+        print(len(tokens))
+        print()
+
+    return all_tokens
 
 
-# make a custom tokenizer
-nlp.tokenizer = WhitespaceTokenizer(nlp.vocab)
-
-# loop over articles, tokenizing each
-for article in generate_articles(paths_to_articles, num_articles=NUM_ARTICLES):
-
-    # tokenize article
-    doc = nlp(article)  # tokenization, tagging, ner, etc...
-
-    tokens = [t.text.strip('.').strip(',').strip(')').strip('(').strip('/').lower() for t in doc]
-    print(tokens)
-    print(len(tokens))
-    print()
-
-
-# retrieve a subset of articles using start and stop
-for article in generate_articles_from_slice(paths_to_articles, start=2, stop=3):
-
-    # tokenize article
-    doc = nlp(article)  # tokenization, tagging, ner, etc...
-
-    tokens = [t.text.strip('.').strip(',').strip(')').strip('(').strip('/').lower() for t in doc]
-    print(tokens)
-    print(len(tokens))
-    print()
+# # retrieve a subset of articles using start and stop
+# for article in generate_articles_from_slice(paths_to_articles, start=2, stop=3):
+#
+#     # tokenize article
+#     doc = nlp(article)  # tokenization, tagging, ner, etc...
+#
+#     tokens = [t.text.lower() for t in doc]
+#     print(tokens)
+#     print(len(tokens))
+#     print()
