df7a43e52f31b783743ed6123ce8bc8113a0bbe5
emilymech <emily.mech@gmail.com>
2019-10-23 12:16:36 -0500

    fix freq sorting, change file name

--- calc_word_counts.py
+++ calc_word_counts.py
@@ -1,12 +1,10 @@
-import matplotlib.pyplot as plt
-import operator
 import pickle
 
 from word_v_world import config
 
 NUM_WORDS = 100
 NUM_LUDWIG_WORKERS = 6
-file_name = 'w2dfs_4800_ALL.pkl'
+file_name = 'w2dfs_4800000_ALL.pkl'
 wiki_param_name = ['param_{}'.format(22 + i) for i in range(NUM_LUDWIG_WORKERS)]
 
 
@@ -44,60 +42,19 @@ def make_master_dict(wiki_param_name):
 
 
 # sort the freq dicts
-# TODO - fix this function once the dicts are figured out
-def sorted_freq_dict(master_dict, NUM_WORDS=NUM_WORDS):
-    sorted_wiki_freqs = (sorted(master_dict.items(), key=operator.itemgetter(1)))[::-1]
-    print(sorted_wiki_freqs)
-    for n, (w, f) in sorted_wiki_freqs:
+def sorted_freq_dict(dict):
+    freq_list = [(dict[key], key) for key in dict]
+    freq_list.sort()
+    freq_list.reverse()
+    for n, pair in enumerate(freq_list):
         if n == NUM_WORDS:
             break
-        print('{:<20} {}'.format(w, f))
-        return sorted_wiki_freqs
-
-
-# make a frequency plot
-def plot(master_freq_dict):
-    word = "at"
-    just_the_occur = []
-    just_the_rank = []
-    word_rank = 0
-    word_frequency = 0
-
-    entry_num = 1
-    for entry in master_freq_dict:
-
-        if entry[0] == word:
-            word_rank = entry_num
-            word_frequency = entry[1]
-
-        just_the_rank.append(entry_num)
-        entry_num += 1
-        just_the_occur.append(entry[1])
-
-    plt.title("Word Frequencies in Wiki Corpus")
-    plt.ylabel("Total Number of Occurrences")
-    plt.xlabel("Rank of word(\"" + word + "\" is rank " + str(word_rank) + ")")
-    plt.loglog(
-        just_the_rank,
-        just_the_occur,
-        basex=10
-    )
-    plt.scatter(
-        [word_rank],
-        [word_frequency],
-        color="orange",
-        marker="*",
-        s=100,
-        label=word
-    )
-    return plt.show()
+        print(str(pair))
+    return freq_list
 
 
 def main():
-    sorted_master_dict = sorted_freq_dict(make_master_dict(wiki_param_name))
-    freq_plot = plot(sorted_freq_dict(make_master_dict(wiki_param_name)))
-    print(sorted_master_dict)
-    print(freq_plot)
+    sorted_freq_dict(make_master_dict(wiki_param_name))
     return
 
 
