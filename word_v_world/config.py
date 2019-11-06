4831f7d12205864ca18fcc87007a0ecb9feb1cdd
Philip Huebner <info@philhuebner.com>
2019-11-06 16:55:29 -0600

    Update config.py

--- word_v_world/config.py
+++ word_v_world/config.py
@@ -1,18 +1,34 @@
 from pathlib import Path
+import sys
+import os
 import socket
 
-# from ludwig.config import mnt_point
+if sys.platform == 'darwin':
+    default_mnt_point = '/Volumes'
+elif sys.platform == 'linux':
+    default_mnt_point = '/media'
+else:
+    if not os.getenv('LUDWIG_MNT'):
+        raise EnvironmentError('Missing LUDWIG_MNT environment variable')
 
 
 class RemoteDirs:
     """
-    Typically, when using LudwigCluster to execute code, research_data should always be /media/research_data
-     regardless of user's OS.
-    However, when user intends to execute code on host only, and intends to retrieve data from shared drive,
-     and has MacOS, root must be changed to something like /Volumes/research_data
+    remote directories are directories not hosted locally, but hosted on the Ludwig file server.
+    the paths defined below are used:
+    1. when jobs are run locally
+    2. when jobs are run on Ludwig workers
+    this means that the paths must be flexible depending on whether 1 or 2 is the case.
+    on ludwig workers, research_data must always evaluate to /media/research_data.
+    however, when run locally, research_data depends on where the file server is mounted.
+    On MacOs for example, research_data evaluates to /Volumes/research_data.
+    On a platform that is not Linux or MacOS, research_data is not automatically defined;
+    Instead, the path to research_data must be manually specified in the environment variable LUDWIG_MNT.
 
+    WARNING: never change the path definitions below.
     """
-    research_data = Path('/media') / 'research_data'
+    mnt_point = os.getenv('LUDWIG_MNT', default_mnt_point)
+    research_data = Path(mnt_point) / 'research_data'
     root = research_data / 'Word_V_World'
     runs = root / 'runs'
 
