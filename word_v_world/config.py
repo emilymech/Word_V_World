4cc0694190f6810fb2c525dd3046503a93a92cb0
phueb <info@philhuebner.com>
2019-11-08 08:57:03 -0600

    no longer need hacky access to local CreateWikiCorpus

--- word_v_world/config.py
+++ word_v_world/config.py
@@ -1,18 +1,8 @@
 from pathlib import Path
-import socket
 
 
 class LocalDirs:
     root = Path(__file__).parent.parent
-
-    # TODO this is a hack - use .env file and dot_env package to remedy
-
-    if socket.gethostname() == 'wirelessprv-10-195-203-206.near.illinois.edu':
-        wiki = Path('/Volumes/GoogleDrive/My Drive/UIUC/PyCharm') / 'CreateWikiCorpus'
-    elif socket.gethostname() == 'Emilys-MacBook-Pro.local':
-        wiki = Path('/Volumes/GoogleDrive/My Drive/UIUC/PyCharm') / 'CreateWikiCorpus'
-    else:
-        wiki = Path('/home/ph/CreateWikiCorpus')
 
 
 class Global:
