d3d4d04444a69ad5fbcd8617e41f82e152beea14
phueb <info@philhuebner.com>
2019-11-08 08:43:00 -0600

    remove test of function that is no longer needed

--- test/article_paths_test.py
+++ test/article_paths_test.py
@@ -4,18 +4,8 @@ import unittest
 class MyTest(unittest.TestCase):
 
     @staticmethod
-    def test_paths():
-        """
-        test that paths to wiki articles are found
-        """
-
-        from word_v_world.articles import get_paths_to_articles
-
-        paths_to_articles = []
-        for p in get_paths_to_articles(param2requests=None):
-            paths_to_articles.append(p)
-
-        return paths_to_articles is not []
+    def some_test():
+        return True
 
 
 if __name__ == '__main__':
