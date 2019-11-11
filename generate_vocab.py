2fc1eed03c77d0cd87bb8dc90fefe3b878dcf68e
phueb <info@philhuebner.com>
2019-11-11 11:13:45 -0600

    move vars to top

--- generate_vocab.py
+++ generate_vocab.py
@@ -3,30 +3,30 @@ from datetime import datetime
 
 from word_v_world import config
 
-vocab_size = 100000
+VOCAB_SIZE = 100000
+NUM_LUDWIG_WORKERS = 6
+W2DF_FILE_NAME = 'w2dfs_4800000_ALL.pkl'
 
 
 def main():
-    wiki_param_names = ['param_{}'.format(22 + i) for i in range(num_ludwig_workers)]
+    wiki_param_names = ['param_{}'.format(22 + i) for i in range(NUM_LUDWIG_WORKERS)]
 
-    w2f = make_master_w2f(wiki_param_names, file_name, vocab_size)
+    w2f = make_master_w2f(wiki_param_names, W2DF_FILE_NAME, VOCAB_SIZE)
 
     # sort
     vocab = sorted(w2f.keys(), key=lambda w: w2f[w], reverse=True)
 
     # create unique vocab identifier
     vocab_name = "_vocab_"
-    event_id = str(vocab_size) + vocab_name + datetime.now().strftime('%Y%m%d_%H-%M-%S')
+    event_id = str(VOCAB_SIZE) + vocab_name + datetime.now().strftime('%Y%m%d_%H-%M-%S')
 
     # save to file
     with (config.LocalDirs.root / 'data' / '{}.txt'.format(event_id)).open('w') as f:
         for n, w in enumerate(vocab):
-            if n == vocab_size:
+            if n == VOCAB_SIZE:
                 break
             f.write(w + '\n')
 
 
 if __name__ == '__main__':
-    num_ludwig_workers = 6
-    file_name = 'w2dfs_4800000_ALL.pkl'
     main()
