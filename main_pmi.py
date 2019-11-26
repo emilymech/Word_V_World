a9ea73e9de67e4dc6dae1ff922c043d9485ae84a
emilymech <emily.mech@gmail.com>
2019-11-26 08:34:48 -0600

    add directions for updating

--- main_pmi.py
+++ main_pmi.py
@@ -1,7 +1,11 @@
 from word_v_world.calculate_pmi import pmi, combine_wf_cf_dicts, get_pair_cf, get_word_freq
 
+''' To Update:
+    1. Update window size'''
+
 # get PMI
 word_freq_dict = get_word_freq()
 pair_cooc_dict = get_pair_cf()
 combined_dict = combine_wf_cf_dicts(word_freq_dict, pair_cooc_dict)
-pmi(combined_dict, 4)
+window_size = 4
+pmi(combined_dict, window_size)
